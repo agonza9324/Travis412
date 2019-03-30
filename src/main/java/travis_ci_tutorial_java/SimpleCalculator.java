6b4cdd3239aa68fd1e7603f8d3c5894235a2baf9
agonza16 <agonza16@students.towson.edu>
2019-03-30 03:03:06 -0400

    Created test cases for division by zero exception handling, as well as added compare() method used to compare two entered integers, and test cases for the method.

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,18 +4,34 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
-	
-	public int subtract(int a, int b){
-		return a-b;
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public int divide(int a, int b) throws DivideByZeroException {
+		if (b == 0) {
+			throw new DivideByZeroException("Cannot divide by zero");
+		}
+		return a / b;
 	}
 	
-	public int multiply(int a, int b){
-		return a*b;
+	public String compare(int a, int b){
+		String result; 
+		
+		if(a>b){
+			result =  ("First number is greater.");
+		}
+		else if (b>a){
+			result = ("Second number is greater.");
+		}
+		else{
+			result = ("Numbers are equal.");
+		}
+		return result;
 	}
-	
-	public int divide(int a, int b){
-		return a/b;
-	}
-	
-	
 }
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -4,29 +4,65 @@ import java.util.*;
 
 import static org.junit.Assert.*;
 import org.junit.*;
+import org.junit.rules.ExpectedException;
 
 public class SimpleCalculatorTest {
+	
+	SimpleCalculator calc;
+	
+	@Before
+	public void setup(){
+		calc = new SimpleCalculator();
+	}
+	
 	@Test
 	public void testAdd() {
-		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
 	
 	@Test
 	public void testSubtraction() {
-		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.subtract(5, 3), 2);
 	}
 	
 	@Test
 	public void testMultiplication() {
-		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.multiply(2, 2), 4);
 	}
 	
 	@Test
-	public void testDivision() {
-		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.divide(2, 2), 1);
+	public void testDivision() throws DivideByZeroException {
+		assertEquals(calc.divide(2, 2), 1);	
 	}
+	
+	@Test(expected =  DivideByZeroException.class)
+	public void testDivisionByZero() throws DivideByZeroException {
+		calc.divide(1, 0);
+	}
+	
+	@Rule
+	public ExpectedException exceptionRule = ExpectedException.none();
+	
+	@Test
+	public void testDivisionByZeroMessage() throws DivideByZeroException {
+		exceptionRule.expect(DivideByZeroException.class);
+	    exceptionRule.expectMessage("Cannot divide by zero");
+		calc.divide(1, 0);
+	}
+	
+	@Test
+	public void testCompare(){
+		assertEquals(calc.compare(8, 5), ("First number is greater."));
+	}
+	
+	@Test
+	public void testCompare2(){
+		assertEquals(calc.compare(3, 5), ("Second number is greater."));
+	}
+	
+	@Test
+	public void testCompare3(){
+		assertEquals(calc.compare(5, 5), ("Numbers are equal."));
+	}
+		
 }
