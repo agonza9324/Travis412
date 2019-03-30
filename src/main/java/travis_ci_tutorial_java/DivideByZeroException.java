59f182d192b82873060fab0fd0c321ed32046671
agonza16 <agonza16@students.towson.edu>
2019-03-30 03:12:00 -0400

    Adding in DivideByZeroException file.

---
+++ src/main/java/travis_ci_tutorial_java/DivideByZeroException.java
@@ -0,0 +1,14 @@
+package travis_ci_tutorial_java;
+
+public class DivideByZeroException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DivideByZeroException(String string) {
+		super(string);
+	}
+
+	
+}
